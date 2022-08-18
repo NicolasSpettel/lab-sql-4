299f85fd9fa333c8ff2700706ea7c13a0aa20146
Nicolas Spettel <nicolas.spettel1991@gmail.com>
2022-08-18 18:31:22 +0200

    Lab SQL Queries 4

---
+++ Lab SQL Queries 4.sql
@@ -0,0 +1,31 @@
+use sakila;
+
+-- Get film ratings.
+select title, rating from film;
+
+-- Get release years.
+select title, release_year from film;
+
+-- Get all films with ARMAGEDDON in the title.
+select title from film where title like '%ARMAGEDDON%';
+
+-- Get all films with APOLLO in the title
+select title from film where title like '%APOLLO%';
+
+-- Get all films which title ends with APOLLO.
+select title from film where title like '%APOLLO';
+
+-- Get all films with word DATE in the title.
+select title from film where title like '%DATE%';
+
+-- Get 10 films with the longest title.
+select title from film order by length(title) desc limit 10;
+
+-- Get 10 the longest films.
+select title,length from film order by film.length desc limit 10;
+
+-- How many films include Behind the Scenes content?
+select count(title) from film where special_features like '%Behind the Scenes%';
+
+-- List films ordered by release year and title in alphabetical order.
+select release_year, title from film order by release_year, title;
